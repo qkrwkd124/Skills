ec5a594a6c17cfc8d07c636f47e554587bf49ec7
qkrwkd124 <qkrwkd120@naver.com>
2025-08-12 16:54:26 +0900

    dataframe , lazyframe 성능비교 ( csv파일 200만건 기준 )

--- Python/polars/df_lf.py
+++ Python/polars/df_lf.py
@@ -6,12 +6,20 @@ csv_path = "/appdata/storage/skils/polars/sales.csv"
 #Eagar(즉시)
 start = time.time()
 df = pl.read_csv(csv_path)
-print(df.head())
+print(f"CSV row count: {df.height}")
 df = df.filter(pl.col("amount") > 100).select(["date", "amount"])
-print(df.head())
 df = df.group_by("date").agg(pl.col("amount").sum())
-print(df.head())
 print("Eager time:", round(time.time() - start, 3), "초")
 
 
 
+start = time.time()
+lf = (
+    pl.scan_csv(csv_path)
+    .filter(pl.col("amount") > 100)
+    .select(["date", "amount"])
+    .group_by("date")
+    .agg(pl.col("amount").sum())
+)
+lf.collect()  # LazyFrame을 실제로 실행하여 DataFrame으로 변환
+print("Lazy time:", round(time.time() - start, 3), "초")
